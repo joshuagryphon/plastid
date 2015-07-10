eb0981b3ec3788b34c8c212be43275380cd4f549
Joshua Griffin Dunn <joshua.g.dunn@gmail.com>
2015-07-09 18:10:56 -0700

    Fixed plural formatting in `get_class_substitutions` and regenerated substitutions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ class Mock(mock.Mock):
             return Mock()
 
 MOCK_MODULES = ['numpy','scipy','matplotlib','pandas','pysam','biopython']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+#sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
 
