92868d7b22974f575ce6207fe8366e6372e45772
Joshua Griffin Dunn <joshua.g.dunn@gmail.com>
2015-10-02 17:03:17 -0700

    setup.py: added --recythonize; cython auto invoked if .c files missing

--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python
+"""Setup script for plastid. This is *fairly* boilerplate EXCEPT:
+
+    1. `sdist`, `build_ext`, `install`, and `develop` commands are wrapped
+       to add a "--recythonize" flag, which causes Cython to regenerate
+       ".c" files from ".pyx" files before the commands are run.
+
+    2.  These commands *also* invoke Cython if the ".c" filses cannot be found.
+
+    3. a `recythonize` command was also created to do this, and only this
+
+    4. a `clean` command was added to wipe old ".c" files
+
+    5. Separately, if the package is being installed on readthedocs.org,
+       it only installs absolutely necessary build dependencies, which are
+       numpy and pysam.
+"""
+
+
 import os
 import sys
 import glob
+#import plastid
 from collections import Iterable
 from setuptools import setup, find_packages, Extension, Command
 from setuptools.command.build_ext import build_ext
 from setuptools.command.install import install
+from setuptools.command.sdist import sdist
+from setuptools.command.develop import develop
 
 NUMPY_VERSION  = "numpy>=1.9"
 SCIPY_VERSION  = "scipy>=0.15.1"
@@ -16,8 +37,8 @@ PYSAM_VERSION  = "pysam>=0.8.3"
 # adding these to setup_requires and/or install_requires doesn't work,
 # because they are needed before them.
 try:
-#    from Cython.Build import cythonize
-#    from Cython.Distutils import build_ext
+    from Cython.Build import cythonize
+    from Cython.Distutils import build_ext
     import numpy
     import pysam
 except ImportError:
@@ -30,75 +51,40 @@ install these via pip, and retry:
 
 
 #===============================================================================
-# Constants/variables used below
+# Simple stuff
 #===============================================================================
-
-use_cython = False
-
-setup_requires = [NUMPY_VERSION,PYSAM_VERSION]
-packages = find_packages()
 
 with open("README.rst") as f:
     long_description = f.read()
 
+version = "0.3.0" # plastid.__version__
+setup_requires = [NUMPY_VERSION,PYSAM_VERSION]
+packages = find_packages()
 
-base_path = os.getcwd()
+def get_scripts():
+    """Detect command-line scripts automatically
+
+    Returns
+    -------
+    list
+        list of strings describing command-line scripts
+    """
+    binscripts = [X.replace(".py","") for X in filter(lambda x: x.endswith(".py") and \
+                                                                "__init__" not in x,
+                                                      os.listdir(os.path.join("plastid","bin")))]
+    return ["%s = plastid.bin.%s:main" % (X,X) for X in binscripts]
 
 
-pyx_paths = [os.path.join(base_path,"plastid","genomics","*.pyx")]
-"""Paths to Cython .pyx files"""
 
-c_paths = glob.glob(os.path.join(base_path,"plastid","genomics","*.c"))
-"""Potential path to cythonized .c files"""
+#===============================================================================
+# Reconfigure build if on readthedocs.org
+#
+# If on doc server, only require essentials for building package:
+#     Cython, numpy, pysam.
+#
+# Everything else can be mocked
+#===============================================================================
 
-# append numpy & pysam headers to include path for gcc & Cython compilers
-ipath = []
-for mod in (numpy,pysam):
-    ipart = mod.get_include()
-    if isinstance(ipart,str):
-        ipath.append(ipart)
-    elif isinstance(ipart,(list,tuple)):
-        ipath.extend(ipart)
-    else:
-        raise ValueError("Could not parse include path: %s" % ipart)
-
-# define extensions for .c files
-ext_modules = [Extension(X.replace(base_path+os.sep,"").replace(".c","").replace(os.sep,"."),
-                        [X],include_dirs=ipath) for X in c_paths]
-
-#ext_modules = [Extension("plastid.genomics.c_common",[base+"/plastid/genomics/c_common.c"],include_dirs=ipath),
-#Extension("plastid.genomics.roitools",[base+"/plastid/genomics/roitools.c"],include_dirs=ipath),
-#Extension("plastid.genomics.map_factories",[base+"/plastid/genomics/map_factories.c"],include_dirs=ipath),
-#]
-
-# if using Cython to regenerate c files:
-if use_cython == True:
-    from Cython.Build import cythonize
-    from Cython.Distutils import build_ext
-    import numpy
-    import pysam
-
-    # required for setup.py to run
-    setup_requires = [CYTHON_VERSION,
-                      PYSAM_VERSION,
-                      NUMPY_VERSION]
-
-    cython_args= {
-        "embedsignature" : True,
-        "language_level" : sys.version_info[0],
-
-        # enable/disable these for development/release
-        #"linetrace" : True, # requires passing CYTHON_TRACE=1 to C compiler
-        #"profile"   : True,
-    }
-    """Cython compiler options"""
-
-    ext_modules = cythonize([Extension("*",pyx_paths,include_dirs = ipath)],
-                             compiler_directives = cython_args,
-                             include_path = ipath)        
-
-# if on doc server, only require Cython, numpy, pysam. Everything else
-# is simulated via mock
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 if not on_rtd:
@@ -113,146 +99,180 @@ else:
     install_requires = ["cython","numpy","pysam"]
 
 
+
 #===============================================================================
-# Helper functions/classes for setup
+# Custom command classes
+#
+# If a user supplies CYTHON_ARG on the command line, these invoke
+# cythonize on .pyx files to regenerate .c files. If not, existing
+# .c files are compiled into .so files or .dlls
 #===============================================================================
 
-def get_scripts():
-    """Detect command-line scripts automatically
+
+
+CYTHONIZE_COMMAND = "recythonize" # command-class command from setup.py
+CYTHONIZE_ARG = "recythonize"     # '--recythonize', passable to sdist, build_ext et c
+
+CYTHON_ARGS= {
+    "embedsignature" : True,
+    "language_level" : sys.version_info[0],
+
+    # enable/disable these for development/release
+    #"linetrace" : True, # requires passing CYTHON_TRACE=1 to C compiler
+    #"profile"   : True,
+}
+
+
+CYTHON_OPTIONS = [
+        (CYTHONIZE_ARG,
+         None,
+         "If supplied, use Cython to regenerate .c sources from pyx files " +\
+         "(requires Cython; default False)"
+        ),
+]
+
+CYTHON_DEFAULTS = [False]
+"""Default values for CYTHON_OPTIONS"""
+
+
+
+# set up include paths for C compilation; this requires numpy & pysam
+INCLUDE_PATH = []
+for mod in (numpy,pysam):
+    ipart = mod.get_include()
+    if isinstance(ipart,str):
+        INCLUDE_PATH.append(ipart)
+    elif isinstance(ipart,(list,tuple)):
+        INCLUDE_PATH.extend(ipart)
+    else:
+        raise ValueError("Could not parse include path: %s" % ipart)
+
+
+# paths to C files and PYX files
+base_path = os.getcwd()
+
+PYX_PATHS = glob.glob(os.path.join(base_path,"plastid","genomics","*.pyx"))
+"""Paths to Cython .pyx files"""
+
+C_PATHS = [X.replace(".pyx",".c") for X in PYX_PATHS]
+"""Potential path to cythonized .c files"""
+
+
+# default extension modules, C files
+ext_modules = [Extension(x.replace(base_path+os.sep,"").replace(".c","").replace(os.sep,"."),
+                    [x],include_dirs=INCLUDE_PATH) for x in C_PATHS]
+
+
+
+
+def wrap_command_classes(baseclass):
+    """Add custom command-line Cython options to setup command classes
+    
+    Parameters
+    ----------
+    baseclass : setuptools.Command or subclass (not instance!)
+        Command class to modify
 
     Returns
     -------
-    list
-        list of strings describing command-line scripts
+    class
+        Modified class
     """
-    binscripts = [X.replace(".py","") for X in filter(lambda x: x.endswith(".py") and \
-                                                                "__init__" not in x,
-                                                      os.listdir(os.path.join("plastid","bin")))]
-    return ["%s = plastid.bin.%s:main" % (X,X) for X in binscripts]
-   
+    class subclass(baseclass):
+        user_options = baseclass.user_options + CYTHON_OPTIONS
+        new_options = CYTHON_OPTIONS
+        new_defaults = CYTHON_DEFAULTS
 
-#class CythonBuildExtProxy(d_build_ext):
-#    """Proxy class to enable use of Cython's `build_ext` command class without
-#    having to import it before Cython is installed
-#    """
-#    def __init__(self,*args,**kwargs):
-#        # import and pull a switcheroo on type
-#        from Cython.Distutils import build_ext
-#        self._obj = build_ext(*args,**kwargs)
-#
-#    def __str__(self):
-#        return str(self._obj)
-#
-#    def __getattr__(self,attr):
-#        return getattr(self._obj,attr)
-#
-#    def __setattr__(self,attr,val):
-#        return setattr(self._obj,attr,val)
-#
-#class LateCython(list):
-#    """Workaround:
-#    
-#    numpy, pysam, and Cython all need to be installed for the .pyx files
-#    to build. Putting these in the `setup()` argument `setup_requires`
-#    is insufficient, because other arguments passed to `setup()`
-#    require numpy, pysam, and Cython, which won't yet be installed.
-#
-#    So, this class delays evaluation of those arguments until all
-#    dependencies in `setup_requires` have been installed, allowing
-#    allowing numpy, pysam, and Cython to be imported, and the pyx files
-#    to be built.
-#
-#    Idea for proxy class from:
-#        https://stackoverflow.com/questions/11010151/distributing-a-shared-library-and-some-c-code-with-a-cython-extension-module/26698408#26698408
-#    """
-#    def __init__(self,ext_paths,cython_kwargs={}):
-#        """
-#        Parameters
-#        ----------
-#        ext_paths : list
-#            List of paths to Cython .pyx files
-#
-#        cython_kwargs : dict, optional
-#            Cython compiler directives
-#        """
-#        self.ext_paths = ext_paths
-#        self.cython_kwargs = cython_kwargs
-#        self._extensions = None
-#
-#    def __getattr__(self,attr):
-#        if attr == "extensions":
-#            if self._extensions is None:
-#                self._cythonize()
-#            return self._extensions
-#
-#    def _cythonize(self):
-#        """Add packages from `setup_requires` to sys.path, and then
-#        cythonize extensions. Only called when contents of `self` are iterated,
-#        i.e. when setuptools is building the package extensions.
-#        """
-#        if self._extensions is None:
-#            try:
-#            # add all eggs from `setup_requires` to sys.path
-#                eggs = glob.glob(".eggs/*egg")
-#                for n,egg in enumerate(eggs):
-#                    print("Adding %s to sys.path" % egg)
-#                    sys.path.append(egg)
-#
-#                # get include paths
-#                import numpy
-#                import pysam
-#                ipath = []
-#                for mod in (numpy,pysam):
-#                    ipart = mod.get_include()
-#                    if isinstance(ipart,str):
-#                        ipath.append(ipart)
-#                    elif isinstance(ipart,Iterable):
-#                        ipath.extend(ipart)
-#                    else:
-#                        raise ValueError("Could not parse include path: %s" % ipart)
-#
-#                # try to build if not yet built
-#                from Cython.Build import cythonize
-#                extensions = [Extension("*",self.ext_paths,include_dirs = ipath)]
-#                b = cythonize(extensions,
-#                              include_path = ipath,
-#                              compiler_directives = self.cython_kwargs)
-#                self._extensions = b
-#
-#                for i in range(n+1):
-#                    sys.path.pop()
-#                return self._extensions
-#            except ImportError:
-#                for i in range(n+1):
-#                    sys.path.pop()
-#
-#        return self._extensions
-#
-#    def __iter__(self):
-#        return iter(self.extensions)
-#        
-#    def __len__(self):
-#        return len(self.ext_paths)
-#
-#    def __getitem__(self,key):
-#        return self.extensions[k]
+        def initialize_options(self):
+            baseclass.initialize_options(self)
+            for (op,_,_), default in zip(self.new_options,
+                    self.new_defaults):
+                setattr(self,op,default)
+        
+        def finalize_options(self):
+            baseclass.finalize_options(self)
+            if "--%s" % CYTHONIZE_ARG in self.distribution.script_args:
+                print("Cythonize on")
+                setattr(self,CYTHONIZE_ARG,True)
+
+        def run(self):
+            have_all = all([os.access(X,os.F_OK) for X in C_PATHS])
+            if have_all == True:
+                print("I have them all!")
+            if have_all == False:
+                print("Could not find .c files. Regenerating via recythonize.")
+                setattr(self,CYTHONIZE_ARG,True)
+            
+            if getattr(self,CYTHONIZE_ARG) == True:
+                print("Cythonizing")
+                self.distribution.script_args.append("--%s" % CYTHONIZE_ARG)
+                self.run_command(CYTHONIZE_COMMAND)
+
+            return baseclass.run(self)
+
+    subclass.__name__ = "cython_%s" % baseclass.__name__
+    return subclass
+
+
+class clean_c_files(Command):
+    """Remove previously generated .c files"""
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        for file_ in C_PATHS:
+            if os.access(file_,os.F_OK):
+                print("clean_c_files: removing %s..." % file_)
+                os.remove(file_)
+
+
+class build_c_from_pyx(build_ext):
+    """Regenerate .c files from pyx files if --CYTHONIZE_ARG or CYTHONIZE_COMMAND is added to command line"""
+    user_options = build_ext.user_options + CYTHON_OPTIONS
+    new_options  = CYTHON_OPTIONS
+    new_defaults = CYTHON_DEFAULTS
+
+    cython_args  = CYTHON_ARGS
+    include_path = INCLUDE_PATH
+    old_extensions = ext_modules
+
+    def initialize_options(self):
+        for (op,_,_), default in zip(self.new_options,
+                self.new_defaults):
+            setattr(self,op,default)
+        build_ext.initialize_options(self)
+
+    def finalize_options(self):
+        if "--%s" % CYTHONIZE_ARG in self.distribution.script_args or CYTHONIZE_COMMAND in self.distribution.script_args:
+            self.run_command('clean')
+            print("build_c_from_pyx: regenerating .c files from Cython")
+            from Cython.Build import cythonize
+            extensions = cythonize(PYX_PATHS,
+                                   include_path=self.include_path,
+                                   compiler_directives=self.cython_args)
+            self.extensions = extensions
+
+        build_ext.finalize_options(self)
+
+    def run(self): # override so that extensions are only build with build_ext, install, etc
+        pass
+
 
 
 #===============================================================================
 # Program body 
 #===============================================================================
 
-class install_after_build(install):
-    def run(self):
-        self.run_command("build_ext")
-
-        return setuptools.command.install.install.run(self)
-
 setup(
 
     # package metadata
     name             = "plastid",
-    version          = "0.3.0", #plastid.__version__,
+    version          = version,
     author           = "Joshua Griffin Dunn",
     author_email     = "joshua.g.dunn@gmail.com",
     maintainer       = "Joshua Griffin Dunn",
@@ -290,19 +310,21 @@ setup(
     packages             = packages,
     #include_package_data = True,
     package_data         = { "" : ["*.pyx","*.pxd","*.c"], },
-#    exclude_package_data = { "" : ["*.c"], },
 #    "": ["*.*"],#
 #     "": ["*.bam","*.bai","*.bed","*.bb","*.gtf","*.gff","*.gz","*.tbi","*.psl",
 #          "*.bowtie","*.fa","*.wig","*.juncs","*.positions","*.sizes","*.as","*.txt"],
 
-    # dependencies, entrypoints, ext modules, et c
     entry_points     = { "console_scripts" : get_scripts()
                        },
-
     ext_modules = ext_modules,
     cmdclass    = {
-                    #'build_ext' : CythonBuildExtProxy,
-                    'build_ext' : build_ext,
+                    CYTHONIZE_COMMAND : build_c_from_pyx,
+                    'build_ext' : wrap_command_classes(build_ext),
+                    'sdist'     : wrap_command_classes(sdist),
+                    'install'   : wrap_command_classes(install),
+                    'develop'   : wrap_command_classes(develop),
+                    'clean'     : clean_c_files,
+
                     #'install' : install_after_build,
                    },
 
