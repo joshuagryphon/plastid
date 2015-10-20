e3e409e348f3b007a99a33bcae4cc6e92bdf1d7a
Joshua Griffin Dunn <joshua.g.dunn@gmail.com>
2015-10-19 20:02:24 -0700

    documentation for plastid.plotting

---
+++ docs/source/generated/plastid.plotting.rst
@@ -0,0 +1,17 @@
+plastid.plotting package
+========================
+
+.. automodule:: plastid.plotting
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+Submodules
+----------
+
+.. toctree::
+
+   plastid.plotting.colors
+   plastid.plotting.plots
+   plastid.plotting.plotutils
+
---
+++ plastid/plotting/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""This subpackage contains plotting utilities to build some compound plots
+not found in matplotlib.
+
+
+    ======================================    =========================================================
+    **Submodule**                             **Contents**
+    --------------------------------------    ---------------------------------------------------------
+    :mod:`plastid.plotting.colors`            Utilities for manipulating, converting, darkening,
+                                              and lightening colors
+
+    :mod:`plastid.plotting.plots`             Plots, such as heatmaps with summary profiles,
+                                              scatter plots with kernel density estimates of 
+                                              marginal distributions, MA plots, et c
+
+    :mod:`plastid.plotting.plotutil`          Utility functions for preprocessing data or manipulating
+                                              axes
+    ======================================    =========================================================
+
