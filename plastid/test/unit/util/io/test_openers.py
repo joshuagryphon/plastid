0ca85cdae52aff52edf73df199565bd1d03caa77
Joshua Griffin Dunn <joshua.g.dunn@gmail.com>
2016-03-30 11:09:17 -0700

    added placeholder unit tests

---
+++ plastid/test/unit/util/io/test_openers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from nose.tools import assert_equal
+
+def test_read_pl_table():
+    assert False
+
+def test_opener():
+    assert False
+    
+def test_get_short_name():
+    assert False
+
+def test_argsopener():
+    assert False
+
+def test_args_to_comment():
+    assert False
