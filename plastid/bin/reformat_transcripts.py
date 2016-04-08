0412936984d0cbf0e0ffa978ccf8a3c385b9b4f0
Joshua Griffin Dunn <joshua.g.dunn@gmail.com>
2016-04-08 15:21:06 -0700

    reformat_transcripts now exports extra columns, gives BigBed conversion advice

--- plastid/bin/reformat_transcripts.py
+++ plastid/bin/reformat_transcripts.py
@@ -21,8 +21,11 @@ import argparse
 import inspect
 import warnings
 import sys
+import os
+import copy
 
 from plastid.util.scriptlib.argparsers import (AnnotationParser,BaseParser)
+from plastid.util.services.exceptions import ArgumentWarning, warn
 from plastid.util.io.openers import argsopener, get_short_name
 from plastid.util.io.filters import NameDateWriter
 from plastid.util.scriptlib.help_formatters import format_module_docstring
@@ -30,6 +33,78 @@ from plastid.util.scriptlib.help_formatters import format_module_docstring
 warnings.simplefilter("once")
 printer = NameDateWriter(get_short_name(inspect.stack()[-1][1]))
 
+MAKE_BIGBED_MESSAGE =\
+"""The BED-formatted file `%(outbase)s.bed` has been exported with extended BED
+columns. Plastid and other software can detect these automatically if you convert
+`%(outbase)s` to a BigBed file using Jim Kent's bedToBigBed utility, as follows:
+
+    $ sort -k1,1 -k2,2n %(outbase)s.bed >%(outbase)s_sorted.bed
+    $ bedToBigBed -tab -type=bed12+%(numcols)s -as fields.as %(outbase)s.bed chrom.sizes %(outbase)s.bb
+
+where `chrom.sizes` is a two-column, tab-delimited table of chromosome name & size,
+and `fields.as` is an autoSql declaration describing the types & names of the extra
+columns. For `%(outbase)s.bed`, `fields.as` could be:
+
+%(autosql)s
+
+Simply edit the type of each custom field to match your data type. Field type
+options are:
+
+    int, uint, short, ushort, byte, ubyte, float, char, string, lstring, enum, set
+
+For details, See the autoSql Grammar specification at:
+
+https://github.com/ENCODE-DCC/kentUtils/blob/36d6274459f644d5400843b8fa097b380b8f7867/src/hg/autoSql/autoSql.doc
+"""
+
+BED12_RESERVED_NAMES = ["chrom","chromStart","chromEnd","name","score","strand",
+                        "thickStart","thickEnd","reserved","blockCount",
+                        "blockSizes","chromStarts"]
+
+DEFAULT_AUTOSQL_STR =\
+"""
+table bigbed_columns "%s columns"
+(
+    string            chrom;          "Chromosome"
+    uint              chromStart;     "chr start"
+    uint              chromEnd;       "chr end"
+    string            name;           "item name"
+    uint              score;          "score"
+    char[1]           strand;         "strand"
+    uint              thickStart;     "thickstart"
+    uint              thickEnd;       "thickend"
+    uint              reserved;       "normally itemRgb"
+    int               blockCount;     "block count"
+    int[blockCount]   blockSizes;     "block sizes"
+    int[blockCount]   chromStarts;    "block starts"
+%s
+)
+"""
+
+AUTOSQL_ROW_FMT_STR = '    string            %s;%s"description of custom field contents"' 
+
+"""
+    float my_floats; "some float values"
+    set(item1,item2,item3,item4) my_sets; "some set options"
+    int     my_ints; "signed integer values"
+    lstring my_strs; "str representation of transcripts"
+    uint[3] my_colors; "r,g,b colors"
+)
+"""
+
+def fix_name(inp,names_used):
+    """Append a number if an autoSql field name is duplicated.
+    """
+    name = inp
+    i = 2
+    while name in names_used:
+        name = "%s%s" % (inp,i)
+        i += 1
+    
+    names_used.append(name)
+    return name
+    
+#: TODO: no functional test for --extra_columns
 def main(argv=sys.argv[1:]): 
     """Command-line program
     
@@ -54,26 +129,50 @@ def main(argv=sys.argv[1:]):
                         help="If specified and output format is GTF2, special characters in column 9 will be escaped (default: True)")
     parser.add_argument("--output_format",choices=["BED","GTF2"],default="GTF2",
                         help="Format of output file. (default: GTF2)")
-    parser.add_argument("outfile",metavar="outfile.gtf",type=str,
+    parser.add_argument("--extra_columns",nargs="+",default=[],type=str,
+                        help="Attributes (e.g. 'gene_id' to output as extra columns in extended BED format (BED output only).")
+    parser.add_argument("outfile",metavar="outfile.[ bed | gtf ]",type=str,
                         help="Output file")
     args = parser.parse_args(argv)
     bp.get_base_ops_from_args(args)
 
+    end_message = ""    
+    extra_cols = args.extra_columns
+    if extra_cols is not None:
+        if args.output_format == "BED":
+            
+            # avoid name clashes
+            names_used = copy.copy(BED12_RESERVED_NAMES)
+            asql_names = [fix_name(X,names_used) for X in extra_cols]
+            autosql_str = "\n".join(AUTOSQL_ROW_FMT_STR % (X," "*max(15-len(X),2)) for X in asql_names)
+            
+            file_info = {
+                "outbase" : args.outfile[:-4],
+                "numcols" : len(extra_cols),
+                "autosql" : DEFAULT_AUTOSQL_STR % (os.path.basename(args.outfile[:-4]),autosql_str),
+                         
+            }
+            end_message = MAKE_BIGBED_MESSAGE % file_info
+        else:
+            warn("`--extra_columns` is ignored for %s-formatted output." % (args.output_format),ArgumentWarning)
+            
+            
     with argsopener(args.outfile,args,"w") as fout:
         c = 0
-        transcripts = ap.get_transcripts_from_args(args)
+        transcripts = ap.get_transcripts_from_args(args,printer=printer)
         
         for transcript in transcripts:
             if args.output_format == "GTF2":
                 fout.write(transcript.as_gtf(escape=args.no_escape))
             elif args.output_format == "BED":
-                fout.write(transcript.as_bed())
+                fout.write(transcript.as_bed(extra_columns=extra_cols))
             if c % 1000 == 1:
                 printer.write("Processed %s transcripts..." % c)
             c += 1
     
     printer.write("Processed %s transcripts total." % c)
     printer.write("Done.")
-
+    print(end_message)
+            
 if __name__ == "__main__":
     main()
