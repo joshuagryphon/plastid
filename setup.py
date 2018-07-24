b46c9f55724d5213439dc902983a05664d3ea80f
Joshua Griffin Dunn <joshua.g.dunn@gmail.com>
2018-07-23 21:48:30 -0400

    cleaned up setup.py; added tox.ini

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 
     2. `build_ext`, `install`, and `develop` commands are wrapped to add
        a ``--recythonize`` flag, which causes Cython to regenerate ".c" files
-       from ".pyx" files before the commands are run. This is merely a 
+       from ".pyx" files before the commands are run. This is merely a
        convenience for developers to force rebuild even if ".c" files
        appear up to date (but are not e.g. due to a switch in git repo)
 
@@ -34,12 +34,12 @@ from pkg_resources import parse_version
 
 plastid_version = "0.4.8"
 
-PYSAM_VER_NUM  = (0,8,4)
-NUMPY_VER_NUM  = (1,9,0)
-CYTHON_VER_NUM = (0,22,0)
+PYSAM_VER_NUM  = (0, 8, 4)
+NUMPY_VER_NUM  = (1, 9, 0)
+CYTHON_VER_NUM = (0, 22, 0)
 
-PYSAM_VER_STR = parse_version(".".join([str(X) for X in PYSAM_VER_NUM]))
-NUMPY_VER_STR = parse_version(".".join([str(X) for X in NUMPY_VER_NUM]))
+PYSAM_VER_STR  = parse_version(".".join([str(X) for X in PYSAM_VER_NUM]))
+NUMPY_VER_STR  = parse_version(".".join([str(X) for X in NUMPY_VER_NUM]))
 CYTHON_VER_STR = parse_version(".".join([str(X) for X in CYTHON_VER_NUM]))
 
 NUMPY_VERSION  = "numpy>=%s.%s.%s"  % NUMPY_VER_NUM
@@ -54,12 +54,11 @@ CWD = os.path.abspath(os.path.dirname(__file__))
 # require python >= 2.7 (for 2.x) or >= 3.3 (for 3.x branch)
 version_message = "plastid requires Python >= 2.7 or >= 3.3. Aborting installation."
 ver = sys.version_info
-if ver < (2,7) or ver[0] == 3 and ver[1] < 3:
+if ver < (2, 7) or ver[0] == 3 and ver[1] < 3:
     raise RuntimeError(version_message)
 
 
-
-# at present, setup/build can't proceed without numpy, Pysam, Cython
+# at present,  etup/build can't proceed without numpy, Pysam, Cython
 # adding these to setup_requires and/or install_requires doesn't work,
 # because they are needed before then.
 foundstr = "(found %s)"
@@ -69,7 +68,7 @@ have_requirements = True
 
 def check_version(modname):
     """Try to import a module by name.
-    
+
     Parameters
     ----------
     modname : str
@@ -78,7 +77,7 @@ def check_version(modname):
     Returns
     -------
     module or `None`
-        imported module if it can be imported, otherwise `None`
+        imported module if it can be imported,  otherwise `None`
     """
     try:
         mod = importlib.import_module(modname)
@@ -102,7 +101,7 @@ try:
     if Cython is not None:
         cythonver = parse_version(Cython.__version__)
         cstr = foundstr % (Cython.__version__)
-        
+
     if numpyver < NUMPY_VER_STR or pysamver < PYSAM_VER_STR or cythonver < CYTHON_VER_STR:
         raise ImportError()
 
@@ -120,17 +119,17 @@ except ImportError:
     ltmp.append(cstr)
 
     print("""
-    
+
 *** IMPORTANT INSTALLATION INFORMATION ***
 
 plastid setup requires the following to be preinstalled:
 
     numpy  %s.%s.%s or greater  %s
-    Pysam  %s.%s.%s or greater  %s 
+    Pysam  %s.%s.%s or greater  %s
     Cython %s.%s.%s or greater  %s
 
 
-Please install or upgrade these via pip, and retry:
+Please install or upgrade these via pip,  and retry:
 
     $ pip install --upgrade numpy pysam cython
     $ pip install plastid
@@ -142,7 +141,7 @@ check the most up-to-date versions by typing:
     $ pip list | grep -E "(Cython|pysam|numpy)+"
 
 If version numbers reported by `pip list` differ from those reported above,
-please check your system to make sure there are not multiple versions 
+please check your system to make sure there are not multiple versions
 of each package installed. This can be a problem e.g. if using conda.
 In this case, Plastid can be installed and run within a virtual environment
 (not a conda environment):
@@ -170,7 +169,7 @@ In this case, Plastid can be installed and run within a virtual environment
 with open("README.rst") as f:
     long_description = f.read()
 
-setup_requires = [NUMPY_VERSION,PYSAM_VERSION,CYTHON_VERSION]
+setup_requires = [NUMPY_VERSION, PYSAM_VERSION, CYTHON_VERSION]
 packages = find_packages()
 
 
@@ -182,10 +181,10 @@ def get_scripts():
     list
         list of strings describing command-line scripts
     """
-    binscripts = [X.replace(".py","") for X in filter(lambda x: x.endswith(".py") and \
+    binscripts = [X.replace(".py", "") for X in filter(lambda x: x.endswith(".py") and \
                                                                 "__init__" not in x,
-                                                      os.listdir(os.path.join("plastid","bin")))]
-    return ["%s = plastid.bin.%s:main" % (X,X) for X in binscripts]
+                                                      os.listdir(os.path.join("plastid",  "bin")))]
+    return ["%s = plastid.bin.%s:main" % (X, X) for X in binscripts]
 
 
 
@@ -198,7 +197,7 @@ def get_scripts():
 # Everything else can be mocked
 #===============================================================================
 
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+on_rtd = os.environ.get('READTHEDOCS',  None) == 'True'
 
 if not on_rtd:
     install_requires = [
@@ -210,7 +209,7 @@ if not on_rtd:
                         "termcolor",
                         ] + setup_requires
 else:
-    install_requires = ["cython","numpy","pysam","biopython","termcolor"]
+    install_requires = ["cython", "numpy", "pysam", "biopython", "termcolor"]
 
 
 
@@ -236,7 +235,7 @@ CYTHON_ARGS= {
 CYTHON_OPTIONS = [
         (CYTHONIZE_ARG,
          None,
-         "If supplied, use Cython to regenerate .c sources from pyx files " +\
+         "If supplied,  use Cython to regenerate .c sources from pyx files " +\
          "(default False)"
         ),
 ]
@@ -246,42 +245,18 @@ CYTHON_DEFAULTS = [False]
 
 
 # add headers from numpy & pysam to include paths for pyx, c files
+# used throughout below
 base_path = os.getcwd()
-
-INCLUDE_PATH = [os.path.join(base_path,"kentUtils","src","inc"),
-                os.path.join(base_path,"kentUtils","samtabix")
-                ]
-
-for mod in (numpy,pysam):
-    ipart = mod.get_include()
-    if isinstance(ipart,str):
-        INCLUDE_PATH.append(ipart)
-    elif isinstance(ipart,(list,tuple)):
-        INCLUDE_PATH.extend(ipart)
-    else:
-        raise ValueError("Could not parse include path: %s" % ipart)
-
-
-# other flags; fill in as needed
-LIBRARIES               = []
-LIBRARY_DIRS            = []
-RUNTIME_LIBRARY_DIRS    = []
-EXTRA_OBJECTS           = []
-DEFINE_MACROS           = pysam.get_defines()
-#EXTRA_LINK_ARGS         = pysam.get_libraries()
-CYTHON_COMPILE_TIME_ENV = {
-    "PYSAM10" : pysam10,
-}
 
 
 #===============================================================================
-# Build an extension of portions of Jim Kent's utilities, which are needed by
+# Build an extension of portions of Jim Kent's utilities,  which are needed by
 # BigWigReader. The Kent utilities come under a permissive license that
-# allows redistribution and modification for any use, including commercial,
-# with the exception of src/portimpl.h, which appears to have its own license.
+# allows redistribution and modification for any use,  including commercial,
+# with the exception of src/portimpl.h,  which appears to have its own license.
 # Sources included here have therefore been modified not to depend on portimpl.h.
 #
-# JK's utilities are compiled into plastid.readers.bigwig, plastid.readers.bigbed,
+# JK's utilities are compiled into plastid.readers.bigwig,  plastid.readers.bigbed,
 # and plastid.readers.bbi_file
 #===============================================================================
 
@@ -336,61 +311,79 @@ kent_sources = [
 ]
 
 
-kent_deps  = [os.path.join(base_path,"kentUtils","samtabix",X) for X in kent_samtabix]
-kent_deps += [os.path.join(base_path,"kentUtils","src","lib",X) for X in kent_sources]
+kent_deps  = [os.path.join(base_path, "kentUtils", "samtabix", X) for X in kent_samtabix]
+kent_deps += [os.path.join(base_path, "kentUtils", "src", "lib", X) for X in kent_sources]
 
 
 #===============================================================================
 # Definition of extension modules
 #===============================================================================
 
+# include path needs to import from kentUtils, numpy, & pysam
 
-# Cython extensions without dependencies on KentUtils
-noinclude_pyx = glob.glob(os.path.join(base_path,"plastid","genomics","*.pyx"))
-ext_modules = [Extension(x.replace(base_path+os.sep,"").replace(".pyx","").replace(os.sep,"."),
-                         [x],
-                         include_dirs         = INCLUDE_PATH,
-                         libraries            = LIBRARIES,
-                         library_dirs         = LIBRARY_DIRS,
-                         runtime_library_dirs = RUNTIME_LIBRARY_DIRS,
-                         extra_objects        = EXTRA_OBJECTS,
-                         cython_directives    = CYTHON_ARGS,
-                         define_macros        = DEFINE_MACROS,
-#                         extra_link_args      = EXTRA_LINK_ARGS,
-                         cython_compile_time_env = CYTHON_COMPILE_TIME_ENV,
+INCLUDE_PATH = [
+    os.path.join(base_path, "kentUtils", "src", "inc"),
+    os.path.join(base_path, "kentUtils", "samtabix"),
+]
+
+for mod in (numpy, pysam):
+    ipart = mod.get_include()
+    if isinstance(ipart, str):
+        INCLUDE_PATH.append(ipart)
+    elif isinstance(ipart, (list, tuple)):
+        INCLUDE_PATH.extend(ipart)
+    else:
+        raise ValueError("Could not parse include path: %s" % ipart)
+
+
+# other flags; fill in as needed
+LIBRARIES     = []
+DEFINE_MACROS = pysam.get_defines()
+
+# Several classes that we cimport moved in pysam 0.10.0,
+# so we define this macro in the build environment
+# to enable map_factories.pyx to cimport from the correct path
+CYTHON_COMPILE_TIME_ENV = {
+    "PYSAM10" : pysam10,
+}
+
+
+extension_kwargs = {
+    "include_dirs"      : INCLUDE_PATH,
+    "language"          : "c",
+    "cython_directives" : CYTHON_ARGS,
+}
+
+# These extensions have no dependencies on kentUtils,
+# but do have dependencies on pysam
+noinclude_pyx = glob.glob(os.path.join(base_path, "plastid", "genomics", "*.pyx"))
+ext_modules = [Extension(x.replace(base_path+os.sep, "").replace(".pyx", "").replace(os.sep, "."),
+                             [x],
+                             libraries               = LIBRARIES,
+                             define_macros           = DEFINE_MACROS,
+                             cython_compile_time_env = CYTHON_COMPILE_TIME_ENV,
+                             **extension_kwargs
                         ) for x in noinclude_pyx]
 
+# The following extensions do link to kentUtils
 bbifile = Extension(
     "plastid.readers.bbifile",
     ["plastid/readers/bbifile.pyx"] + kent_deps,
-    language="c",
-    include_dirs=INCLUDE_PATH,
     libraries=LIBRARIES + ["z"],
-    library_dirs=LIBRARY_DIRS,
-    runtime_library_dirs=RUNTIME_LIBRARY_DIRS,
-    cython_directives=CYTHON_ARGS,
+    **extension_kwargs
 )
 
 bigwig = Extension(
     "plastid.readers.bigwig",
     ["plastid/readers/bigwig.pyx"] + kent_deps,
-    language="c",
-    include_dirs=INCLUDE_PATH,
     libraries=LIBRARIES + ["z"],
-    library_dirs=LIBRARY_DIRS,
-    runtime_library_dirs=RUNTIME_LIBRARY_DIRS,
-    cython_directives=CYTHON_ARGS,
+    **extension_kwargs
 )
 
 bigbed = Extension(
     "plastid.readers.bigbed",
     ["plastid/readers/bigbed.pyx"] + kent_deps,
-    language="c",
-    include_dirs=INCLUDE_PATH,
-    libraries=LIBRARIES + ["z"],
-    library_dirs=LIBRARY_DIRS,
-    runtime_library_dirs=RUNTIME_LIBRARY_DIRS,
-    cython_directives=CYTHON_ARGS,
+    **extension_kwargs
 )
 
 ext_modules.append(bbifile)
@@ -403,7 +396,7 @@ PYX_PATHS = []
 for ex in ext_modules:
     PYX_PATHS.extend([X for X in ex.sources if X.endswith("pyx")])
 
-C_PATHS   = [X.replace(".pyx",".c") for X in PYX_PATHS]
+C_PATHS = [X.replace(".pyx", ".c") for X in PYX_PATHS]
 
 
 # classes & functions for compilation -----------------------------------------
@@ -411,7 +404,7 @@ C_PATHS   = [X.replace(".pyx",".c") for X in PYX_PATHS]
 def wrap_command_classes(baseclass):
     """Add custom command-line `--recythonize` options to distutils/setuptools
     command classes
-    
+
     Parameters
     ----------
     baseclass : setuptools.Command or subclass (not instance!)
@@ -429,24 +422,24 @@ def wrap_command_classes(baseclass):
 
         def initialize_options(self):
             baseclass.initialize_options(self)
-            for (op,_,_), default in zip(self.new_options,
+            for (op, _, _),  default in zip(self.new_options,
                     self.new_defaults):
-                setattr(self,op,default)
-        
+                setattr(self, op, default)
+
         def finalize_options(self):
             baseclass.finalize_options(self)
             if "--%s" % CYTHONIZE_ARG in self.distribution.script_args:
                 print("Cythonize on")
-                setattr(self,CYTHONIZE_ARG,True)
+                setattr(self, CYTHONIZE_ARG, True)
 
         def run(self):
-            have_all = all([os.access(X,os.F_OK) for X in C_PATHS])
+            have_all = all([os.access(X, os.F_OK) for X in C_PATHS])
             if have_all == False:
                 print("Could not find .c files. Regenerating via recythonize.")
-                setattr(self,CYTHONIZE_ARG,True)
+                setattr(self, CYTHONIZE_ARG, True)
                 self.distribution.script_args.append("--%s" % CYTHONIZE_ARG)
-            
-            if getattr(self,CYTHONIZE_ARG) == True:
+
+            if getattr(self, CYTHONIZE_ARG) == True:
                 print("Cythonizing")
                 self.run_command(CYTHONIZE_COMMAND)
 
@@ -460,20 +453,20 @@ class clean_c_files(Command):
     """Remove previously generated .c files"""
     user_options = []
     description = "Remove previously generated .c files"
- 
+
     def initialize_options(self):
         pass
- 
+
     def finalize_options(self):
         pass
- 
+
     def run(self):
         for file_ in C_PATHS:
-            if os.access(file_,os.F_OK):
+            if os.access(file_, os.F_OK):
                 print("clean_c_files: removing %s ..." % file_)
                 os.remove(file_)
 
- 
+
 class build_c_from_pyx(build_ext):
     """Regenerate .c files from pyx files if --CYTHONIZE_ARG or CYTHONIZE_COMMAND is added to command line"""
     user_options = build_ext.user_options + CYTHON_OPTIONS
@@ -487,41 +480,44 @@ class build_c_from_pyx(build_ext):
     description = "Regenerate .c files from .pyx source"
 
     def initialize_options(self):
-        for (op,_,_), default in zip(self.new_options,
+        for (op, _, _),  default in zip(self.new_options,
                 self.new_defaults):
-            setattr(self,op,default)
+            setattr(self, op, default)
         build_ext.initialize_options(self)
 
     def finalize_options(self):
-        if "--%s" % CYTHONIZE_ARG in self.distribution.script_args or CYTHONIZE_COMMAND in self.distribution.script_args:
+        if "--%s" % CYTHONIZE_ARG in self.distribution.script_args \
+          or CYTHONIZE_COMMAND in self.distribution.script_args:
+
             self.run_command('clean')
             print("build_c_from_pyx: regenerating .c files from Cython")
             extensions = cythonize(ext_modules,
-                                   compiler_directives     = CYTHON_ARGS,
-                                   compile_time_env = CYTHON_COMPILE_TIME_ENV,
+                                   compiler_directives = CYTHON_ARGS,
+                                   compile_time_env    = CYTHON_COMPILE_TIME_ENV,
                                    )
             self.extensions = extensions
 
         build_ext.finalize_options(self)
 
-    def run(self): # override so that extensions are only built with build_ext, install, etc
+    # override so that extensions are only built with build_ext, install, etc
+    def run(self):
         pass
 
 
 
 #===============================================================================
-# Program body 
+# Program body
 #===============================================================================
 
 FAIL_MSG = \
-"""Setup failed. If this is due to a compiler error, try rebuilding the Cython
-sources. If installing via pip, make sure all dependencies are already
-pre-installed, then separately run:
+"""Setup failed. If this is due to a compiler error,  try rebuilding the Cython
+sources. If installing via pip,  make sure all dependencies are already
+pre-installed,  then separately run:
 
     $ pip install --verbose --upgrade --install-option="--recythonize" plastid | tee install_log.txt
 
 
-Or, if installing manually via setup.py:
+Or,  if installing manually via setup.py:
 
     $ python setup.py install --recythonize | tee install_log.txt
 """
@@ -543,7 +539,7 @@ setup(
     url              = "https://github.com/joshuagryphon/plastid",
     download_url     = "https://pypi.python.org/pypi/plastid/",
     platforms        = "OS Independent",
- 
+
     classifiers      = [
          'Development Status :: 5 - Production/Stable',
 
@@ -572,31 +568,31 @@ setup(
                            "kentUtils.src.lib",
                            "kentUtils.samtabix"],
 
-    package_data = { 
-        "" : ["*.pyx","*.pxd"],
+    package_data = {
+        "" : ["*.pyx", "*.pxd"],
         "kentUtils"          : ["README*"],
         "kentUtils.src.inc"  : ["*.h"],
         "kentUtils.src.lib"  : kent_sources + ["README"],
-        "kentUtils.samtabix" : kent_samtabix + ["*.h","COPYING","README*","AUTHORS"],
+        "kentUtils.samtabix" : kent_samtabix + ["*.h", "COPYING", "README*", "AUTHORS"],
     },
 
     package_dir = {
         "plastid"            : "plastid",
         "kentUtils"          : "kentUtils",
-        "kentUtils.src.inc"  : os.path.join("kentUtils","src","inc"),
-        "kentUtils.src.lib"  : os.path.join("kentUtils","src","lib"),
-        "kentUtils.samtabix" : os.path.join("kentUtils","samtabix"),
+        "kentUtils.src.inc"  : os.path.join("kentUtils", "src", "inc"),
+        "kentUtils.src.lib"  : os.path.join("kentUtils", "src", "lib"),
+        "kentUtils.samtabix" : os.path.join("kentUtils", "samtabix"),
     },
- 
+
     entry_points = {
         "console_scripts" : get_scripts()
     },
 
     ext_modules = ext_modules,
-    
+
     cmdclass    = {
         CYTHONIZE_COMMAND : build_c_from_pyx,
-        'build_ext' : wrap_command_classes(build_ext), 
+        'build_ext' : wrap_command_classes(build_ext),
         'install'   : wrap_command_classes(install),
         'develop'   : wrap_command_classes(develop),
         'clean'     : clean_c_files,
@@ -604,7 +600,7 @@ setup(
 
     setup_requires   = setup_requires,
     install_requires = install_requires,
-   
+
     tests_require     = [ "nose>=1.0" ],
     test_suite        = "nose.collector",
 
