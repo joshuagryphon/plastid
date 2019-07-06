d809b7cd67af0156332e0458c5ede14474bf8ee2
Joshua Griffin Dunn <joshua.g.dunn@gmail.com>
2019-07-06 16:32:39 -0400

    doc updates

--- plastid/test/common.py
+++ plastid/test/common.py
@@ -2,5 +2,12 @@
 from numpy.testing import suppress_warnings
 from plastid.util.services.exceptions import DataWarning
 
+#===============================================================================
+# Warnings suppression
+#
+# Use within bodies of test functions as e.g. `with sup_data: foo`
+# Do NOT use as function decorators, as these will hose `yield` tests
+#===============================================================================
+
 sup_data = suppress_warnings()
 sup_data.filter(category=DataWarning)
